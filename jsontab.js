73e37a1803143b267c0f9ed771a104494c7a5225
vg <vgorsunov@kodeks.net>
2021-02-13 15:26:43 +0400

    Script for viewing JSON as a table

---
+++ jsontab.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env nodejs
+
+/*
+  Prints every leaf value in JSON file on a single line prepended by all the
+  keys leading to it. Pipe through grep to find out which keys contain 
+  matching values
+*/
+
+const fs = require('fs');
+
+printTable();
+
+function printTable() {
+	if(process.argv.length <= 2) {
+		console.error(`USAGE: ${process.argv[1]} JSON_FILE ...`);
+		process.exit(1);
+	}
+	for(let i = 2; i < process.argv.length; i++) {
+		printFile(process.argv[i]); 
+	}
+}
+
+function printFile(path) {
+	fs.readFile(path, function(e, content) {
+		if(e) {
+			console.error(path, e);
+			return;
+		}
+		try {
+			var d = JSON.parse(content.toString("utf-8"));
+		} catch(e) {
+			console.error(path, e);
+			return;
+		}
+		printObject([path], d);
+	});
+}
+
+function printObject(prefix, d) {
+	try {
+		if(typeof d != "string") {
+			for(var k in d)
+				printObject(prefix.concat(k), d[k]);
+			return;
+		}
+	} catch(e) {}
+	console.log(prefix.join(" "), d);
+}
