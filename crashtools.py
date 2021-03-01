692e0fddaf421cce888ec9a8d17b23df73397b08
vg <gorsunov@gmail.com>
2021-03-01 19:33:29 +0400

    Symbol download working

--- crashtools.py
+++ crashtools.py
@@ -8,6 +8,15 @@ def check(error):
 	if error.fail:
 		raise Exception(str(error))
 
+def cut(s, limit):
+	return len(s) > (limit+3) and "%s..." % s[:limit] or s
+
+def getId(m):
+	return "".join(str(m.uuid).split("-")).upper() + "0"
+
+def num(n):
+	return ",".join(re.findall(r"\d{1,3}", str(n)[::-1]))[::-1]
+
 class Count:
 	def __init__(self):
 		self.absent = 0
@@ -16,30 +25,112 @@ class Count:
 		self.system = 0
 		self.store = 0
 
+class WorkCount:
+	def __init__(self, todo=0):
+		self.done = 0
+		self.total = todo
+
 class Util:
 	def verb(self, message):
 		if not self.args.verbose:
 			return
+		self.print(message)
+
+	def print(self, message, count=None):
+		count = count or hasattr(self, "count") and self.count
+		if count:
+			sys.stderr.write("\r[%s / %s] " % (
+				num(count.done), num(count.total)))
 		sys.stderr.write(message)
 		sys.stderr.flush()
+
+	def setVar(self, name, value):
+		old = lldb.SBDebugger.GetInternalVariableValue(
+			name, self.debugger.GetInstanceName())
+		check(lldb.SBDebugger.SetInternalVariable(
+			name, value, self.debugger.GetInstanceName()))
+		self.verb("Variable '%s' was set to '%s' (was '%s')\n" % (
+			name, value, " ".join(old)))
 
 class SymbolNotOnServer(Exception):
 	pass
 
 class Target(Util):
-	def __init__(self, path, args, debugger):
-		self.path, self.args, self.debugger = path, args, debugger
+	def __init__(self, args, debugger):
+		self.args, self.debugger = args, debugger
 		self.error = lldb.SBError()
 		self.target = self.check(self.debugger.CreateTarget(
 			"", "", "", True, self.error))
+		# Hack: add mapping to nonexistent files so
+		# ModuleList::GetSharedModule gets called first without a real path
+		# and finds storage files in target.exec-search-paths
+		if self.args.storage:
+			self.check(self.target.AppendImageSearchPath(
+				"/Volumes", self.args.storage, self.error))
+			self.check(self.target.AppendImageSearchPath(
+				"/Applications", self.args.storage, self.error))
+		if self.args.download:
+			self.setVar("target.preload-symbols", "false")
+			
+	def load(self, path):
+		self.path = path
 		self.verb("Loading '%s'..." % self.path)
 		self.process = self.check(self.target.LoadCore(path, self.error))
 		self.verb("DONE\n")
+		return self
+
+	def attach(self, name):
+		self.verb("Attaching '%s'..." % name)
+		self.process = self.check(self.target.Attach(
+			lldb.SBAttachInfo(name, False), self.error))
+		self.verb("DONE\n")
+		return self
 
 	def check(self, result):
 		if self.error.fail:
 			raise Exception(str(self.error))
 		return result
+
+	def getName(self, m):
+		if not m.IsValid():
+			return "-"
+		if m.file.fullpath.startswith(self.args.storage):
+			return m.file.fullpath
+		parts = m.file.fullpath.split(os.sep)
+		if len(parts) <= 1:
+			return m.file.fullpath
+		return "/%s/.../%s" % (parts[1], parts[-1])
+
+	def getSymbolFileName(self, m):
+		if m.GetSymbolFileSpec() == m.file:
+			return "-"
+		return m.GetSymbolFileSpec()
+
+	def getSourceLinePrefix(self, f):
+		if not f.line_entry.IsValid():
+			return " "
+		cwd = os.getcwd().split(os.sep)[-1]
+		parts = f.line_entry.file.fullpath.split(os.sep)
+		try:
+			path = os.sep.join(parts[parts.index(cwd) + 1:])
+		except ValueError:
+			path = f.line_entry.file.fullpath
+		return "%s:%d:" % (path, f.line_entry.line)
+
+	def printStacks(self):
+		for t in self.process.threads:
+			print("%3d %8d ================= %d '%s'" % (
+				t.idx, t.id, t.stop_reason, t.GetStopDescription(80)))
+			for f in t.frames:
+				print("%s %3d %3d %16X '%s' %s (%s)" % (
+					self.getSourceLinePrefix(f), 
+					t.idx, f.idx, f.pc, f.addr.symbol.name,
+					self.getName(f.addr.module),
+					self.getSymbolFileName(f.addr.module)))
+				# arguments, locals, statics, in_scope_only
+				for v in f.GetVariables(True, True, False, True):
+					print("name='%s' loc='%s' val='%s' type='%s' (%d)" % (
+						v.name, v.location, v.value, v.type.name, v.type.size))
 
 	def printRegions(self):
 		regions = self.process.GetMemoryRegions()
@@ -58,6 +149,50 @@ class Target(Util):
 				(r.IsMapped() and "m" or "."),
 				r.GetRegionBase(), r.GetRegionEnd() - r.GetRegionBase(),
 				name))
+
+	def replaceModuleFromStorage(self, m):
+		for ver in os.listdir(self.args.storage):
+			path = os.path.join(self.args.storage, ver, m.file.basename)
+			if not os.path.isfile(path):
+				continue
+			# path, architecture, UUID, symfile
+			new = self.target.AddModule(path, None, None, None)
+			if not new:
+				raise Exception("%s not added" % path)
+			if new.uuid != m.uuid:
+				self.verb("Skipping %s (%s) for '%s'\n" % (
+					new.file.fullpath, new.uuid, m.file.basename))
+				self.target.RemoveModule(new)
+				continue
+			if m.num_sections != new.num_sections:
+				self.verb(
+					"Module %s: num_sections differ,\n%d in %s\n%d in %s" % (
+						m.uuid, m.num_sections, m.file.fullpath,
+						new.num_sections, new.file.fullpath))
+				self.target.RemoveModule(new)
+				continue
+			for i in range(m.num_sections):
+				check(self.target.SetSectionLoadAddress(
+					new.sections[i],
+					m.sections[i].GetLoadAddress(self.target)))
+			self.target.RemoveModule(m)
+			return 
+		msg = ("'%s' (%s) not in storage" % (
+			m.file.fullpath, m.uuid))
+		if self.args.unchecked:
+			self.verb(msg)
+		else:
+			raise Exception(msg)
+
+	def fixModulesNotInStorage(self):
+		# some modules get removed
+		for m in self.target.modules[:]:
+			if m.file.fullpath.startswith(self.args.storage):
+				continue
+			if m.file.fullpath.startswith("/Volumes"):
+				self.replaceModuleFromStorage(m)
+			if m.file.fullpath.startswith("/Applications"):
+				self.replaceModuleFromStorage(m)
 
 	def printModules(self, tool):
 		count = Count()
@@ -87,20 +222,23 @@ class Target(Util):
 					(self.args.stat and m.uuid or self.path), name))
 				continue
 			elif self.args.modules:
-				print("%d %08X %s %016X '%s' '%s'" % (
-					m.num_sections, fsize, self.getId(m),
+				print("%d %08X %s %016X '%s' %s" % (
+					m.num_sections, fsize, getId(m),
 					self.getSomeLoadAddress(m), name,
-					m.GetSymbolFileSpec()))
+					(m.GetSymbolFileSpec() == m.file) and
+					"-" or m.GetSymbolFileSpec()))
 		if self.args.stat:
 			if not hasattr(tool, "statHeaderPrinted"):
 				print(" modules: total, system, store, other, absent")
 				tool.statHeaderPrinted = True
 			print(
-				self.getTime() + "%3d %3d %3d %3d %3d %s %s" % (
+				self.getTime() + "%3d %3d %3d %3d %3d %11s %11s %s '%s'" % (
 					self.target.GetNumModules(),
 					count.system, count.store, count.other, 
-					count.absent, self.path,
-					self.target.executable.basename))
+					count.absent, cut(self.path, 8),
+					cut(self.target.executable.basename, 8),
+					self.process.selected_thread.frames[0].addr,
+					self.process.selected_thread.GetStopDescription(80)))
 
 	def getSomeLoadAddress(self, m):
 		for s in m.sections:
@@ -108,43 +246,111 @@ class Target(Util):
 				break
 		return s.GetLoadAddress(self.target)
 
-	def getId(self, m):
-		return "".join(str(m.uuid).split("-")).upper() + "0"
-
 	def getTime(self):
 		mtime = time.localtime(os.path.getmtime(self.path))
 		return time.strftime("%Y-%m-%d %H:%M| ", mtime)
 
-	def downloadSymbols(self):
+	def queueDownloadSymbols(self, server):
+		if not self.args.storage:
+			raise Exception("Can't download without storage dir")
+		work = []
 		for m in self.target.modules:
+			if not m.file.fullpath.startswith(self.args.storage):
+				continue
 			self.verb("Full=%s\n" % m.file.fullpath)
-			self.downloadTemplate("%s%s/%s/%s.dSYM.tar.bz2", m)
-			self.downloadTemplate("%s%s/%s/%s.tar.bz2", m)
+			work.append(DownloadTemplate(
+				server, "%s/%s/%s.dSYM.tar.bz2", m))
+			if 0: # found this in 1 file on gh, but it never worked
+				work.append(DownloadTemplate(
+					server, "%s/%s/%s.tar.bz2", m))
+		return work
 
-	def downloadTemplate(self, tmpl, m):
-		prefix = self.args.download
-		if not prefix.endswith("/"):
-			prefix += "/"
-		url = tmpl % (
-			prefix, m.file.basename, self.getId(m), m.file.basename)
+
+class DownloadTemplate:
+	def __init__(self, server, tmpl, m):
+		self.server = server
+		self.template = tmpl
+		self.module = m
+		
+	def run(self, tool):
+		m = self.module
+		url = self.server.getUrl(self.template % (
+			m.file.basename, getId(m), m.file.basename))
+		name = url.split("/")[-1].split(".tar.bz2")[0]
+		expected = os.path.join(m.file.dirname, name)
+		if os.path.isdir(expected):
+			tool.verb("Already have %s\n" % expected)
+			return
+		# Better have private tmp for each version so nothing gets mixed
+		tmp = os.path.join(m.file.dirname, "tmp")
+		if not os.path.isdir(tmp):
+			os.mkdir(tmp)
 		try:
-			self.download(url, useCurl=True)
+			self.server.download(url, tmp, tool, useCurl=True)
 		except SymbolNotOnServer:
 			print("%s not found" % url)
+			return
+		self.rename(os.path.join(tmp, name), expected, tool)
 
-	def download(self, url, useCurl=False):
-		self.verb("Downloading %s ...\n" % url)
-		import bz2
-		c = bz2.BZ2Decompressor()
+	def rename(self, src, dst, tool):
+		tool.verb("Renaming %s -> %s" % (src, dst))
+		os.rename(src, dst)
+		
+
+class Server:
+	def __init__(self, args):
+		self.args = args
+		self.hasResumableDownloads = True
+		
+	def startCurlWithResumeEnabled(self, url, dir):
+		name = url.split(os.sep)[-1]
+		curl = subprocess.Popen(
+			["curl", "-L", "-C", "-", url, "-o", name],	cwd=dir)
+		return (curl, name)
+
+	def shortenFile(self, path, amount):
+		with open(path, "+b") as f:
+			f.truncate(os.path.getsize(path) - amount)
+
+	def getUrl(self, path):
+		prefix = self.args.download
+		if not prefix.endswith("/"):
+			if not path.startswith("/"):
+				prefix += "/"
+		return prefix + path
+
+	def download(self, url, dir, tool, useCurl=False, tarBz2=True):
+		tool.print("Downloading %s -> %s..." % (url, dir))
+		if not tarBz2:
+			import bz2
+			bz2dec = bz2.BZ2Decompressor()
+		count = None
 		if useCurl:
-			curl = subprocess.Popen([
-				"curl", "-L", url], stdout=subprocess.PIPE)
-			src = curl.stdout
+			if self.hasResumableDownloads:
+				curl, name = self.startCurlWithResumeEnabled(url, dir)
+				if curl.wait() == 33:
+					# Some servers break if we have full content...
+					self.shortenFile(os.path.join(dir, name), 1)
+					curl, _ = self.startCurlWithResumeEnabled(url, dir)
+				if curl.wait() == 33:
+					self.hasResumableDownloads = False
+				elif curl.wait() != 0:
+					raise Exception("curl.wait() = %d", curl.wait())
+				src = open(os.path.join(dir, name), "rb")
+				count = WorkCount(os.path.getsize(os.path.join(dir, name)))
+			if not self.hasResumableDownloads:
+				curl = subprocess.Popen(
+					["curl", "-L", url], stdout=subprocess.PIPE)
+				src = curl.stdout
 		else:
 			src = urllib.request.urlopen(url)
-			if src.getcode() != 200:
+			if src.getcode() == 404:
+				raise SymbolNotOnServer(src.getcode())
+			elif src.getcode() != 200:
 				raise Exception("src.getcode() = %d" % src.getcode())
-		tar = subprocess.Popen(["tar", "t"], stdin=subprocess.PIPE)
+		tar = subprocess.Popen(
+			["tar", "xv%s" % (tarBz2 and "j" or "")],
+			stdin=subprocess.PIPE, cwd=dir)
 		while True:
 			p = src.read(16384)
 			if not p:
@@ -155,55 +361,71 @@ class Target(Util):
 				message = None
 			if message == "Symbol Not Found":
 				raise SymbolNotOnServer(message)
-			p = c.decompress(p)
+			if count:
+				count.done += len(p)
+			tool.print("unpacking...", count)
+			if not tarBz2:
+				p = bz2dec.decompress(p)
 			try:
 				tar.stdin.write(p)
 			except EOFError:
 				pass # tar exited?
 		tar.stdin.close()
-		if useCurl and curl.wait() != 0:
-			raise Exception("curl.wait() = %d", curl.wait())
 		if tar.wait() != 0:
 			raise Exception("tar.wait() = %d", tar.wait())
+
 
 class Tool(Util):
 	def __init__(self, args, debugger):
 		self.args, self.debugger = args, debugger
+		self.work = []
 		if self.debugger is None:
 			self.verb("Starting debugger...")
 			self.debugger = lldb.SBDebugger.Create()
 			self.verb("DONE name=%s\n" % self.debugger.GetInstanceName())
+		# This is faster than replacing modules in fixModulesNotInStorage
 		if self.args.storage:
 			dirs = [os.path.join(self.args.storage, x) for x in os.listdir(
 				self.args.storage) if not x.startswith(".")]
-			self.verb("dirs=%s\n" % " ".join(dirs))
-			check(lldb.SBDebugger.SetInternalVariable(
-				"target.exec-search-paths", " ".join(dirs),
-				self.debugger.GetInstanceName()))
-		if self.args.verbose:
-			# doesn't check
+			self.setVar("target.exec-search-paths", " ".join(dirs))
+		if self.args.dverbose:
+			# doesn't check categories
 			if not self.debugger.EnableLog("lldb", ["dyld"]):
 				raise Exception("Bad log")
 		
 	def run(self):
-		self.args.DMPFILE.sort(key=os.path.getmtime, reverse=True)
 		# self.verb("Press ENTER...")
 		# sys.stdin.read(1)
+		if self.args.attachname:
+			self.handleTarget(Target(self.args, self.debugger).attach(
+				self.args.attachname))
+		self.args.DMPFILE.sort(key=os.path.getmtime, reverse=True)
 		for path in self.args.DMPFILE:
-			t = Target(path, self.args, self.debugger)
-			if self.args.download:
-				t.downloadSymbols()
-			elif self.args.modules or self.args.absent:
-				t.printModules(self)
-			elif not self.args.stat:
-				t.printRegions()
-			if not (self.args.modules or self.args.absent):
-				if self.args.stat:
-			   		t.printModules(self)
+			t = Target(self.args, self.debugger).load(path)
+			if 0 and self.args.storage:
+				t.fixModulesNotInStorage()
+			self.handleTarget(t)
+		self.count = WorkCount(len(self.work))
+		for task in self.work:
+			task.run(self)
+			self.count.done += 1
+
+	def handleTarget(self, t):
+		if self.args.download:
+			self.work += t.queueDownloadSymbols(Server(self.args))
+		elif self.args.threads:
+			t.printStacks()
+		elif self.args.modules or self.args.absent:
+			t.printModules(self)
+		elif not self.args.stat:
+			t.printRegions()
+		if not (self.args.modules or self.args.absent):
+			if self.args.stat:
+			   	t.printModules(self)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description=__doc__)
-	parser.add_argument("DMPFILE", help="Path to core file", nargs="+")
+	parser.add_argument("DMPFILE", help="Path to core file", nargs="*")
 	parser.add_argument(
 		"-m", "--modules", action="store_true", help="List binaries")
 	parser.add_argument(
@@ -215,5 +437,14 @@ if __name__ == "__main__":
 		"-d", "--download", help="Symbol server URL to download symbols from")
 	parser.add_argument(
 		"-r", "--storage", help="Path to binary archive")
+	parser.add_argument(
+		"-u", "--unchecked", action="store_true",
+		help="Allow binaries not in archive")
+	parser.add_argument(
+		"-n", "--attachname", help="Attach to a live process by name")
+	parser.add_argument(
+		"-t", "--threads", action="store_true", help="Print stacks")
 	parser.add_argument("-v", "--verbose", action="store_true")
+	parser.add_argument(
+		"-b", "--dverbose", action="store_true", help="LLDB verbose output")
 	Tool(parser.parse_args(), None).run()
