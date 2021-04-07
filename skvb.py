78cdb25fd54f54ff6e26c8ad73a8c3ddd6a6935c
vg <vgorsunov@kodeks.net>
2021-04-07 22:44:04 +0500

    Script to send keys

---
+++ skvb.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+from __future__ import print_function
+"Send keys to VirtualBox VM"
+import sys, subprocess, argparse, os, re, time
+
+class Row:
+    def __init__(self, base, lower, upper):
+        self.base = base
+        self.lower = lower
+        self.upper = upper
+
+class Modifier:
+    def __init__(self, code):
+        self.code = code
+        self.on = False
+
+rows = [
+    Row(2, "1234567890-=", "!@#$%^&*()_+"),
+    Row(16, "qwertyuiop[]\n", "QWERTYUIOP{}"),
+    Row(30, "asdfghjkl;'", "ASDFGHJKL:\""),
+    Row(43, "\\zxcvbnm,./", "|ZXCVBNM<>?"),
+    Row(57, " ", ""),
+]
+
+class Tool:
+    def __init__(self, args):
+        self.args = args
+        self.vmName = self.args.machine
+        self.shift = Modifier(42)
+
+    def sendCode(self, code):
+        subprocess.check_output([
+            "VBoxManage", "controlvm", self.vmName, "keyboardputscancode",
+            "%02X" % code])
+        time.sleep(0.01)
+
+    def setModifier(self, mod, target):
+        if mod.on != target:
+            sys.stdout.write(target and u'\u2191' or u'\u2193')
+            self.sendCode(target and mod.code or (mod.code + 128))
+            mod.on = target
+        return self
+
+    def run(self):
+        if not self.vmName:
+            rvms = subprocess.check_output([
+                "VBoxManage", "list", "runningvms"]).strip().split("\n")
+            if len(rvms) != 1:
+                raise Exception("rvms=%s" % rvms)
+            self.vmName = re.match("\"(.+)\"", rvms[0]).group(1)
+        inp = " ".join(self.args.INPUT) + "\n"
+        for m in re.finditer(r".", inp, re.DOTALL):
+            self.sendChar(m.group(0))
+        self.setModifier(self.shift, False)
+
+    def findInRowAndSend(self, row, line, shiftValue, c):
+        try:
+            i = line.index(c)
+        except ValueError:
+            return False
+        self.setModifier(self.shift, shiftValue)
+        sys.stdout.write(c)
+        self.sendCode(row.base + i)
+        self.sendCode(row.base + i + 128)
+        return True
+
+    def sendChar(self, c):
+        for row in rows:
+            if self.findInRowAndSend(row, row.lower, False, c) or\
+               self.findInRowAndSend(row, row.upper, True, c):
+                return
+        raise Exception("Character '%s' not found" % c)
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("-m", "--machine", help="Target machine")
+parser.add_argument("INPUT", nargs="+")
+Tool(parser.parse_args()).run()
