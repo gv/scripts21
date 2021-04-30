9012acca541218ac05741aab072d917dbbeafb2d
vg <vgorsunov@kodeks.net>
2021-05-01 01:11:33 +0400

    changed chmod to chown...

--- rundisk_vb.sh
+++ rundisk_vb.sh
@@ -11,19 +11,20 @@ vd=~/"$name.vmdk"
 set -xe
 VBoxManage unregistervm --delete "$name" || true
 rm -vf "$vd"
-sudo chmod g+rw "$path"
+#sudo chmod g+rw "$path"
+sudo chown "$(whoami)" "$path"
 VBoxManage internalcommands createrawvmdk\
 		   -filename "$vd" -rawdisk "$path"
 VBoxManage createvm --register --name "$name" 
 VBoxManage storagectl "$name" --name "$ctrl" --add sata
 VBoxManage storageattach "$name" --storagectl "$ctrl"\
 		   --port 0 --device 0 --type hdd --medium "$vd"
+# --groups removed cause it didn't clean up VM files properly
 VBoxManage modifyvm "$name"\
 		   --firmware efi\
-		   --memory 1536 --acpi on --boot1 disk\
+		   --memory 2048 --acpi on --boot1 disk\
 		   --audio none --nic1 null --nictype1 virtio --mouse usbtablet\
 		   --cableconnected1 on\
 		   --ioapic on\
-		   --groups "/$(basename "$0")"\
 		   --description "Updated by $0 on $(date)"
 VBoxManage startvm "$name"
