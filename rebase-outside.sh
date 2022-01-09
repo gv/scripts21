70e3da759d8c96c84e14740d5ce5b7654d3de701
vg <vgorsunov@kodeks.net>
2022-01-09 12:53:18 +0400

    Changed rebase-outside to using git worktree

--- rebase-outside.sh
+++ rebase-outside.sh
@@ -1,24 +1,22 @@
 #!/bin/bash -e
-if [ $# != 2 ]; then
+if [ $# == 0 -o $# == 1 ]; then
 	echo "\
 rebase-outside: rebase a branch using out of place working copy
-USAGE: $0 BRANCH BASE"
+USAGE: $0 BRANCH BASE [REBASE_OPTIONS]"
 	exit 1
 fi
 project=$(basename $(pwd))
 src=$(pwd)
 name=$1
 base=$2
+shift
+shift
 work=../work.$project
 set -xe
 test -d "$work" ||\
-	(git clone . "$work.tmp" -b "$name" --depth 1 && mv "$work.tmp" "$work")
-# Go into "detached head" so we can fetch into any branch name
-(cd "$work" && git checkout @^ &&\
-	 git fetch -v "$src" "+$name:$name" &&\
-	 git fetch -v "$src" "+$base:$base" &&\
+	(git worktree add "$work.tmp" "$name" && mv "$work.tmp" "$work")
+(cd "$work" && git rebase --abort || true;\
 	 git checkout "$name" &&\
-	 git rebase "$base")
-git fetch -v "$work" "+$name:last"
-git fetch -v "$work" "+$name:$name"
-
+	 git rebase "$base" "$@" &&\
+	 # Cannot check out in the main dir when it's checked out in here
+	 git checkout --detach)
