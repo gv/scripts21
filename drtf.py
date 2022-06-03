a241c330e81487c017afb597cb005f58c5001767
vg <vg@karote.localdomain>
2022-06-03 12:53:01 +0400

    Script for latvian letters; final level check for drtf.py

--- drtf.py
+++ drtf.py
@@ -17,24 +17,25 @@ class RtfFile:
         return bytes([int(m.group(1), 16)]).decode("windows-1251")
 
     def load(self):
-        level = 0
+        self.level = 0
         text = " ".join(open(self.path).read().split("\n"))
         for m in re.finditer("([{}])([^{}]*)", text):
             b, t = m.group(1, 2)
             if b == "{":
-                level += 1
+                self.level += 1
             else:
-                level -= 1
+                self.level -= 1
             if t != "":
                 t = re.sub("\\\\'([0-9a-fA-F]{2})", self.decodeChar, t)
-                self.blocks.append(RtfBlock(level, t))
+                self.blocks.append(RtfBlock(self.level, t))
         return self
 
     def print(self):
         for b in self.blocks:
             head = " " * (b.level - 1) if b.level < 10 else\
                 "%d       " % b.level
-            print(head + b.text)
+            print("%s'%s'" % (head, b.text))
+        print("Final level:%d" % self.level)
                 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("INPUT")
