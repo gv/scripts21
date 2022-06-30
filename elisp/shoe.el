8450a0d933e42c59463f3b5925adaf03c4977774
vg <vgorsunov@kodeks.net>
2022-06-30 14:37:40 +0400

    Function move-file

--- elisp/shoe.el
+++ elisp/shoe.el
@@ -421,7 +421,29 @@ next grep is started"
 	(setq buffer-read-only t)
 	;; [ ] add message to buffer when process is done; use "compile"
 	;;     or vc-run-delayed
-	(pop-to-buffer bn)))
+   (pop-to-buffer bn)))
+
+;; This one is from https://zck.org/emacs-move-file
+(defun move-file (new-location)
+ "Write this file to NEW-LOCATION, and delete the old one."
+ (interactive (list (expand-file-name
+					 (if buffer-file-name
+					  (read-file-name "Move file to: ")
+					  (read-file-name "Move file to: "
+					   default-directory
+					   (expand-file-name (file-name-nondirectory (buffer-name))
+						default-directory))))))
+ (when (file-exists-p new-location)
+  (delete-file new-location))
+ (let ((old-location (expand-file-name (buffer-file-name))))
+  (message "old file is %s and new file is %s"
+   old-location
+   new-location)
+  (write-file new-location t)
+  (when (and old-location
+		 (file-exists-p new-location)
+		 (not (string-equal old-location new-location)))
+   (delete-file old-location))))
 
 (setq tramp-mode nil)
 
